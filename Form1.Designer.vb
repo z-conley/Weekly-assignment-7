a533e90bd816c1a1d05e01bf292c7bdc739b263a
Zachary Conley <conley_zachary@ad1.columbusstate.edu>
2023-04-04 17:48:02 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Frmkaraoke
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Lblkaraoke = New System.Windows.Forms.Label()
+        Me.CboChoices = New System.Windows.Forms.ComboBox()
+        Me.Btntotal = New System.Windows.Forms.Button()
+        Me.Btnclear = New System.Windows.Forms.Button()
+        Me.Lblhourly = New System.Windows.Forms.Label()
+        Me.Txthours = New System.Windows.Forms.TextBox()
+        Me.Lblcostdisplay = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Lblkaraoke
+        '
+        Me.Lblkaraoke.AutoSize = True
+        Me.Lblkaraoke.BackColor = System.Drawing.Color.Black
+        Me.Lblkaraoke.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblkaraoke.ForeColor = System.Drawing.Color.White
+        Me.Lblkaraoke.Location = New System.Drawing.Point(411, 40)
+        Me.Lblkaraoke.Name = "Lblkaraoke"
+        Me.Lblkaraoke.Size = New System.Drawing.Size(306, 36)
+        Me.Lblkaraoke.TabIndex = 0
+        Me.Lblkaraoke.Text = "Karaoke Music night"
+        '
+        'CboChoices
+        '
+        Me.CboChoices.BackColor = System.Drawing.Color.Black
+        Me.CboChoices.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CboChoices.ForeColor = System.Drawing.Color.White
+        Me.CboChoices.FormattingEnabled = True
+        Me.CboChoices.Items.AddRange(New Object() {"Number of songs($2.99 per song)", "Private karaoke room($8.99 an hour)"})
+        Me.CboChoices.Location = New System.Drawing.Point(379, 101)
+        Me.CboChoices.Name = "CboChoices"
+        Me.CboChoices.Size = New System.Drawing.Size(371, 27)
+        Me.CboChoices.TabIndex = 1
+        Me.CboChoices.Text = "Select an option:"
+        '
+        'Btntotal
+        '
+        Me.Btntotal.BackColor = System.Drawing.Color.Black
+        Me.Btntotal.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btntotal.ForeColor = System.Drawing.Color.White
+        Me.Btntotal.Location = New System.Drawing.Point(373, 375)
+        Me.Btntotal.Name = "Btntotal"
+        Me.Btntotal.Size = New System.Drawing.Size(164, 43)
+        Me.Btntotal.TabIndex = 2
+        Me.Btntotal.Text = "Total cost"
+        Me.Btntotal.UseVisualStyleBackColor = False
+        '
+        'Btnclear
+        '
+        Me.Btnclear.BackColor = System.Drawing.Color.Black
+        Me.Btnclear.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnclear.ForeColor = System.Drawing.Color.White
+        Me.Btnclear.Location = New System.Drawing.Point(580, 374)
+        Me.Btnclear.Name = "Btnclear"
+        Me.Btnclear.Size = New System.Drawing.Size(164, 43)
+        Me.Btnclear.TabIndex = 3
+        Me.Btnclear.Text = "Clear form"
+        Me.Btnclear.UseVisualStyleBackColor = False
+        '
+        'Lblhourly
+        '
+        Me.Lblhourly.BackColor = System.Drawing.Color.Black
+        Me.Lblhourly.Font = New System.Drawing.Font("Times New Roman", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblhourly.ForeColor = System.Drawing.Color.White
+        Me.Lblhourly.Location = New System.Drawing.Point(385, 197)
+        Me.Lblhourly.Name = "Lblhourly"
+        Me.Lblhourly.Size = New System.Drawing.Size(359, 33)
+        Me.Lblhourly.TabIndex = 4
+        '
+        'Txthours
+        '
+        Me.Txthours.BackColor = System.Drawing.Color.Black
+        Me.Txthours.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txthours.ForeColor = System.Drawing.Color.White
+        Me.Txthours.Location = New System.Drawing.Point(532, 259)
+        Me.Txthours.Name = "Txthours"
+        Me.Txthours.Size = New System.Drawing.Size(42, 29)
+        Me.Txthours.TabIndex = 5
+        Me.Txthours.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Lblcostdisplay
+        '
+        Me.Lblcostdisplay.BackColor = System.Drawing.Color.Black
+        Me.Lblcostdisplay.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblcostdisplay.ForeColor = System.Drawing.Color.White
+        Me.Lblcostdisplay.Location = New System.Drawing.Point(373, 454)
+        Me.Lblcostdisplay.Name = "Lblcostdisplay"
+        Me.Lblcostdisplay.Size = New System.Drawing.Size(371, 35)
+        Me.Lblcostdisplay.TabIndex = 6
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(339, -1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(462, 503)
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'Frmkaraoke
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImage = Global.Weekly_assignment_7.My.Resources.Resources.karaoke
+        Me.ClientSize = New System.Drawing.Size(800, 498)
+        Me.Controls.Add(Me.Lblcostdisplay)
+        Me.Controls.Add(Me.Lblhourly)
+        Me.Controls.Add(Me.Lblkaraoke)
+        Me.Controls.Add(Me.Btnclear)
+        Me.Controls.Add(Me.Btntotal)
+        Me.Controls.Add(Me.Txthours)
+        Me.Controls.Add(Me.CboChoices)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.Name = "Frmkaraoke"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Karaoke night"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Lblkaraoke As Label
+    Friend WithEvents CboChoices As ComboBox
+    Friend WithEvents Btntotal As Button
+    Friend WithEvents Btnclear As Button
+    Friend WithEvents Lblhourly As Label
+    Friend WithEvents Txthours As TextBox
+    Friend WithEvents Lblcostdisplay As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,122 @@
+﻿' Program name: Karaoke Music night
+' Name: Zachary Conley
+' Date: 4/4/2023
+' Purpose: This app is used to find the prices for a karaoke music night
+
+Option Strict On
+
+Public Class Frmkaraoke
+
+    Private _decsong As Decimal = 2.99D
+    Private _decHourlyrate As Decimal = 8.99D
+    Private Sub Frmkaraoke_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' When loading the form, the splash screen stays up for 6 seconds
+        Threading.Thread.Sleep(3000)
+        ClearForm()
+
+    End Sub
+
+
+    Private Function InputValidate() As Boolean
+        ' This function is used for the msgboxes which display error messages for when the user inputs incorrect options
+        Dim Number As Integer
+        Dim blnValid As Boolean = False
+        Try
+            Number = Convert.ToInt32(Txthours.Text)
+            If Number > 0D Then
+                blnValid = True
+                Return blnValid
+            Else
+                MsgBox("Please enter a number that is greater than zero", , "Error")
+            End If
+        Catch exception As FormatException
+            MsgBox("Please enter an amount that is valid", , "Error")
+        Catch exception As OverflowException
+            MsgBox("Please enter a reasonable amount of hours", , "Error")
+        Catch excerption As SystemException
+            MsgBox("Entry is invalid, please enter a valid number", , "Error")
+
+        End Try
+        Txthours.Focus()
+        Txthours.Clear()
+        Return blnValid
+
+    End Function
+
+    Private Function Roomcost(ByVal intpass As Integer) As Decimal
+        ' This function serves as the multiplication of the hourly rate for the number inputed by the user
+        Dim decTicket As Decimal
+        decTicket = intpass * _decHourlyrate
+        Return decTicket
+
+    End Function
+
+    Private Function Songcost(ByVal intpass As Integer) As Decimal
+        ' this used the song rate and multiplies it by the users input
+        Dim decSong As Decimal
+        decSong = intpass * _decsong
+        Return decSong
+
+    End Function
+
+    Private Sub Btnclear_Click(sender As Object, e As EventArgs) Handles Btnclear.Click
+        ClearForm()
+
+    End Sub
+
+
+    Private Sub ClearForm()
+        ' The clearform event takes everything off of the screen, clears the choice box and then inputs select an option: back in
+        Lblcostdisplay.Visible = False
+        Txthours.Visible = False
+        Btntotal.Visible = False
+        Btnclear.Visible = False
+        Lblhourly.Visible = False
+        Txthours.Clear()
+        CboChoices.SelectedIndex = -1
+        CboChoices.Text = "Select an option:"
+
+    End Sub
+
+    Private Sub Btntotal_Click(sender As Object, e As EventArgs) Handles Btntotal.Click
+        'This is the conversion for the button total click event which takes the songcost and roomcost depending on the user choice
+        Dim blnAmountvalid As Boolean = False
+        Dim intvalue As Integer
+        Dim decTot As Decimal
+
+        blnAmountvalid = InputValidate()
+        If blnAmountvalid = True Then
+            intvalue = Convert.ToInt32(Txthours.Text)
+            If CboChoices.SelectedIndex = 0 Then
+                decTot = Songcost(intvalue)
+            Else
+                decTot = Roomcost(intvalue)
+            End If
+
+            Lblcostdisplay.Visible = True
+            Lblcostdisplay.Text = "Total cost of karaoke night - " & decTot.ToString("C")
+
+        End If
+    End Sub
+
+    Private Sub CboChoices_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CboChoices.SelectedIndexChanged
+        If CboChoices.SelectedIndex = 0 Then
+            Lblhourly.Text = "Number of karaoke songs:"
+            Lblhourly.Visible = True
+            Txthours.Visible = True
+            Btntotal.Visible = True
+            Btnclear.Visible = True
+            Txthours.Focus()
+
+        End If
+        If CboChoices.SelectedIndex = 1 Then
+            Lblhourly.Text = "Hourly rental of karaoke room:"
+            Lblhourly.Visible = True
+            Txthours.Visible = True
+            Btnclear.Visible = True
+            Btntotal.Visible = True
+            Txthours.Focus()
+
+        End If
+    End Sub
+End Class
